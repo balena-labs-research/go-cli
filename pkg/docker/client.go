fcbfccf65a1f9665416024a2a16db0b8ed63949f
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2023-02-16 11:48:36 +0000

    Rename docker sdk to client

--- pkg/docker/client.go
+++ pkg/docker/client.go
@@ -6,7 +6,6 @@ import (
 
 // Create a new connection to the Docker daemon.
 func NewClient(ip string, port string) (*client.Client, error) {
-
 	tcpAddress := "tcp://" + ip + ":" + port
 	cli, err := client.NewClientWithOpts(client.WithHost(tcpAddress), client.WithAPIVersionNegotiation())
 
