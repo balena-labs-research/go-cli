b6aceb1b9ff295ad27b050707d229aaaca6a9b8c
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2023-02-20 10:14:33 +0000

    Add Mutagen support

--- cmd/go-cli/cobra/commands.go
+++ cmd/go-cli/cobra/commands.go
@@ -2,6 +2,9 @@ package cobra
 
 import (
 	"github.com/balena-labs-research/go-cli/cmd/go-cli/devices"
+	"github.com/mutagen-io/mutagen/cmd"
+	"github.com/mutagen-io/mutagen/cmd/mutagen/daemon"
+	"github.com/mutagen-io/mutagen/cmd/mutagen/sync"
 	"github.com/spf13/cobra"
 )
 
@@ -33,13 +36,71 @@ var logsCmd = &cobra.Command{
 // scanCmd represents the scan command
 var lookupAddressCmd = &cobra.Command{
 	Use:    "lookup",
-	Short:  "Find devices on the network by reverse lookup. Requires a query to all devices in your network IP range.",
-	Long:   "Uses a reverse lookup on all IP ranges in the range `your-systems-subnet/24`. Specify your own range by passing a range as an argument: 192.168.1.0/24.",
+	Short:  "Find devices on the network by reverse lookup. Requires a query to all devices in your network IP range",
+	Long:   "Uses a reverse lookup on all IP ranges in the range `your-systems-subnet/24`. Specify your own range by passing a range as an argument: 192.168.1.0/24",
 	PreRun: toggleDebug,
 	Args:   cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		devices.Scan("lookup", args)
 	},
+}
+
+var mount = &cobra.Command{
+	Use:          "mount",
+	Short:        "For mounting balena containers to your local system",
+	SilenceUsage: true,
+}
+
+var mutagenCreate = &cobra.Command{
+	Use:          "create <container_name> <container_path> <local_path> <host_address>",
+	Short:        "Create a new synchronised mount session with a balena container",
+	PreRun:       toggleDebug,
+	RunE:         sync.CreateMain,
+	SilenceUsage: true,
+}
+
+var mutagenList = &cobra.Command{
+	Use:          "list [<session>...]",
+	Short:        "List existing synchronization sessions and their statuses",
+	PreRun:       toggleDebug,
+	RunE:         sync.ListMain,
+	SilenceUsage: true,
+}
+
+// runCommand is the run command.
+var mutagenRun = &cobra.Command{
+	Use:          "run",
+	Short:        "Run the Mutagen daemon",
+	Args:         cmd.DisallowArguments,
+	Hidden:       true,
+	RunE:         daemon.RunMain,
+	SilenceUsage: true,
+}
+
+var mutagenTerminate = &cobra.Command{
+	Use:          "terminate [<session>...]",
+	Short:        "Permanently terminate a synchronization session",
+	PreRun:       toggleDebug,
+	RunE:         sync.TerminateMain,
+	SilenceUsage: true,
+}
+
+// stopCommand is the stop command.
+var mutagenStop = &cobra.Command{
+	Use:          "stop",
+	Short:        "Stop the Mutagen daemon if it's running",
+	Args:         cmd.DisallowArguments,
+	RunE:         daemon.StopMain,
+	SilenceUsage: true,
+}
+
+// startCommand is the start command.
+var mutagenStart = &cobra.Command{
+	Use:          "start",
+	Short:        "Start the Mutagen daemon if it's not already running",
+	Args:         cmd.DisallowArguments,
+	RunE:         daemon.StartMain,
+	SilenceUsage: true,
 }
 
 // sshCmd represents the ssh command
@@ -66,4 +127,11 @@ func init() {
 	rootCmd.AddCommand(lookupAddressCmd)
 	rootCmd.AddCommand(sshCmd)
 	sshCmd.Flags().StringVarP(&port, "port", "p", "22222", "Specify ssh port. Default is 22222")
+	rootCmd.AddCommand(mount)
+	mount.AddCommand(mutagenCreate)
+	mount.AddCommand(mutagenList)
+	mount.AddCommand(mutagenRun)
+	mount.AddCommand(mutagenTerminate)
+	mount.AddCommand(mutagenStop)
+	mount.AddCommand(mutagenStart)
 }
--- go.mod
+++ go.mod
@@ -1,21 +1,39 @@
 module github.com/balena-labs-research/go-cli
 
-go 1.18
+go 1.19
 
 require (
 	github.com/docker/docker v20.10.17+incompatible
 	github.com/google/gopacket v1.1.19
 	github.com/spf13/cobra v1.5.0
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b
 )
 
 require (
+	github.com/bmatcuk/doublestar/v4 v4.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/eknkc/basex v1.0.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-logr/logr v0.4.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hectane/go-acl v0.0.0-20190604041725-da78bae5fc95 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
-	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	github.com/mutagen-io/extstat v0.0.0-20210224131814-32fa3f057fa8 // indirect
+	github.com/mutagen-io/fsevents v0.0.0-20180903111129-10556809b434 // indirect
+	github.com/mutagen-io/gopass v0.0.0-20230214181532-d4b7cdfe054c // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230119192704-9d59e20e5cd1 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.21.3 // indirect
+	k8s.io/klog/v2 v2.8.0 // indirect
 )
 
 require (
@@ -25,17 +43,21 @@ require (
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/mutagen-io/mutagen v0.16.5
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	gotest.tools/v3 v3.3.0 // indirect
 )
+
+replace github.com/mutagen-io/mutagen => github.com/maggie0002/mutagen v0.16.6-0.20230217194235-73964f0841c4
